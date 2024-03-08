a04994254e82041678510db3532c534cdc0b8e93
YagnikManav <yagnik.c@ahduni.edu.in>
2024-03-09 01:14:06 +0530

    IMPLEMENTING SAGA AS IT IS LESS CONFUSING

--- src/app/Redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice';
-
-const rootReducer = combineReducers({
-  counter: counterReducer
-  // Add more reducers here if needed
-});
-
-export default rootReducer;
