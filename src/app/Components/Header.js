e0a6bc68124c1ed2e5e0cc39d6a021bd3eaa80c9
YagnikManav <yagnik.c@ahduni.edu.in>
2024-03-12 13:45:52 +0530

    revert to first thing

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -1,37 +1,23 @@
-'use client'
 import React from 'react'
 import Image from 'next/image';
 import "../globals.css"
-import { addToCart, emptyCart, remove_from_cart } from '../Redux/redux/action';
-import { useDispatch } from 'react-redux'
-import { productList, productSearchByCategory } from '../Redux/redux/productAction';
-import { useSelector } from 'react-redux'
-import { useEffect } from 'react';
 
 const Header = () => {
-  const dispatch = useDispatch();
-  let data = useSelector((state) => state.productData);
-  console.log(data)
-
   return (
-    <div className=''>
-    {
-      data.map((item) => <div className=''>
-        <img src={item.photo} alt="" width={'150px'} height={'170px'} />
-        <div className=''>  {item.name} </div>
-        <div>Color : {item.color} </div>
-        <div>Price : {item.price} </div>
-        <div>Category : {item.category} </div>
-        <div>Brand : {item.brand} </div>
-        <div>
-          <button className="" onClick={() => dispatch(addToCart(item))} >Add to Cart</button>
-          <button className="" onClick={() => dispatch(remove_from_cart(item.id))}>Remove </button>
-
-        </div>
-      </div>)
-    }
-  </div>
-  
+    <div className='header'>
+       
+       <div className='header-img'>
+      <Image
+        src="/asset/car.jpg"
+        alt="Logo"
+        width={100}
+        height={100}
+      />
+      </div>
+     
+   
+      <div className='header-text'>Library Rover</div>
+    </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,13 @@
-'use client'
-import Header from "./Components/Header";
-import React, { useEffect } from 'react';
-import Main from "./Components/Main";
-// import { useSelector, useDispatch } from 'react-redux';
-// import { fetchUser } from './Redux/counterSlice';
-import { Provider } from "react-redux";
-import store from "./Redux/redux/store";
 
+import Header from "./Components/Header";
 export default function Home() {
- 
   return (
-    <Provider store={store}>
-    <div>
-     
-   
+    <main className="">
      <Header />
      
 
-   
-
-   
       
-   
-
-    </div>
-    // </Provider>
+      
+    </main>
   );
 }
