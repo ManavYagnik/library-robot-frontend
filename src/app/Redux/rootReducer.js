12881adbf0814dd7850aabcc2dbed450fccf06ab
YagnikManav <yagnik.c@ahduni.edu.in>
2024-03-13 14:28:32 +0530

    some changes

--- src/app/Redux/rootReducer.js
+++ src/app/Redux/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import { cartData } from "./reducer";
 import { productData } from "./productReducer";
-import { contactData } from "./contactReducer";
+
 import { isloading } from "./LoadingReducer";
 
 export default combineReducers({
